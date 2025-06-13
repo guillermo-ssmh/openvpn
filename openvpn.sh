f8af56ac81e40ed5e2d4a071e768ff88b3c90187
guillermo-ssmh <guillermo.ssmh@gmail.com>
2025-06-13 10:51:35 +0200

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -14,12 +14,14 @@ podman run -d \
 sleep 3
 
 podman exec openvpn-as /usr/sbin/sacli --user "openvpn" --new_pass "R00tu53r"   SetLocalPassword
-podman exec openvpn-as /usr/sbin/sacli --key "admin_ui.https.port"      --value "944"  ConfigPut
-podman exec openvpn-as /usr/sbin/sacli --key "cs.https.port"            --value "944"  ConfigPut
-podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.0.listen.port" --value "1195" ConfigPut 
-podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.0.proto"       --value "udp"  ConfigPut 
-podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.1.listen.port" --value "4344" ConfigPut 
-podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.1.proto"       --value "tcp"  ConfigPut
+podman exec openvpn-as /usr/sbin/sacli --key "admin_ui.https.port"        --value "944"  ConfigPut
+podman exec openvpn-as /usr/sbin/sacli --key "cs.https.port"              --value "944"  ConfigPut
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.server.daemon.tcp.port" --value "4334" ConfigPut 
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.server.daemon.udp.port" --value "1195" ConfigPut 
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.0.listen.port"   --value "1195" ConfigPut 
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.0.proto"         --value "udp"  ConfigPut 
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.1.listen.port"   --value "4344" ConfigPut 
+podman exec openvpn-as /usr/sbin/sacli --key "vpn.daemon.1.proto"         --value "tcp"  ConfigPut
 podman exec openvpn-as /usr/sbin/sacli start
 
 podman ps -l
