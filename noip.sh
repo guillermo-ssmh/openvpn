4881081bc9b1e5c32fcff42a49d6da63e79c8b5b
Guillermo SSMH <guillermo.ssmh@gmail.com>
2025-06-25 00:00:09 +0200

    added noip client

---
+++ noip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Stopping...." 
+podman stop noip
+
+echo "Dropping...."
+podman rm noip
+
+echo "Creating container"
+docker run -d --env-file noip.env --name noip ghcr.io/noipcom/noip-duc:latest
+
+sleep 1
+
+echo "Checking...."
+podman inspect --format '{{.State.Status}}' noip 
+
+exit 0
